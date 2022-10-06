335ff8807b026172ef73794ae7350e73e7fa5d86
praveshstark <pravesh.yadav.virat@gmail.com>
2022-10-06 11:20:34 +0530

    Fixed comment button UI

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,27 +1,88 @@
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { AuthService } from '../services/auth.service';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   loginForm: any;
+//   title: any;
+
+//   constructor(
+//     private fb: FormBuilder,
+//     private router: Router,
+//     private authService: AuthService,
+//   ) {
+//     if (this.authService.isLoggedIn()) {
+//       this.router.navigateByUrl('/home');
+//     }
+  
+//   }
+
+//   ngOnInit() {
+//     this.title = 'Administrator Login';
+//     this.createForm();
+//   }
+
+//   createForm() {
+//     this.loginForm = this.fb.group({
+//       email: ['',[Validators.required,Validators.email]],
+//       password: ['',[Validators.required,Validators.minLength(6)]]
+//     })
+//   }
+
+ 
+//   get f() { return this.loginForm.controls }
+//   onSubmit() {
+//     this.authService.login({
+//       email: this.loginForm.get('email').value,
+//       password: this.loginForm.get('password').value
+//     }).subscribe(
+//       result => {
+//         if (result) {
+//           this.router.navigateByUrl('/home');
+//           console.log("logged in");
+//         }
+//       }
+//     )
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormsModule,
+  FormControl,
+  NgForm,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm: any;
   title: any;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService,
+    private authService: AuthService
   ) {
     if (this.authService.isLoggedIn()) {
       this.router.navigateByUrl('/home');
     }
-  
   }
 
   ngOnInit() {
@@ -29,27 +90,61 @@ export class LoginComponent implements OnInit {
     this.createForm();
   }
 
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  // onLogin() {
+  //   console.log(this.loginForm.value);
+  //   this.authService.login({
+  //     email: this.loginForm.get('email').value,
+  //     password: this.loginForm.get('password').value,
+  //   }).subscribe((result) => {
+  //     // if (result) {
+  //     //   this.router.navigateByUrl('/home');
+  //     // }
+  //   const token = this.authService.getCurrentUser();
+  //   console.log(token)
+  //   if (token) {
+  //     localStorage.setItem('token', token.userName);
+  //     console.log('Login Successful');
+  //     this.router.navigate(['/']);
+  //   } else {
+  //     console.log('Login Failed');
+  //   }
+  //   });
+    
+  // }
+
   createForm() {
     this.loginForm = this.fb.group({
-      email: [''],
-      password: ['']
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
- 
-  get f() { return this.loginForm.controls }
   onSubmit() {
-    this.authService.login({
-      email: this.loginForm.get('email').value,
-      password: this.loginForm.get('password').value
-    }).subscribe(
-      result => {
+    this.authService
+      .login({
+        email: this.loginForm.get('email').value,
+        password: this.loginForm.get('password').value,
+      })
+      .subscribe((result) => {
         if (result) {
-          this.router.navigateByUrl('/home');
-          console.log("logged in");
+        let token = this.authService.getCurrentUser();
+        console.log(token)
+        if(token){
+          token = this.authService.getDecodeToken(token)
         }
-      }
-    )
+        console.log(token)
+        if (token) {
+          localStorage.setItem('token', token.username);
+          console.log('Login Successful');
+          this.router.navigate(['/']);
+        } else {
+          console.log('Login Failed');
+        }
+        }
+      });
   }
-
 }
--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
+
+// @NgModule({
+//   imports: [NgbModule],
+// })
+// export class YourAppModule {
+// }
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,15 +17,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  //   loginForm=new FormGroup({
+  //   email:new FormControl('',[Validators.required,Validators.email]),
+  //   password:new FormControl('',[Validators.required,Validators.minLength(8)]),
+  // })
+
+  // showCard: boolean;
+
+  // loginUser()
+  // {
+  //   console.warn(this.loginForm.value);
+  // }
+  // get email()
+  // {
+  //   return this.loginForm.get('user')
+  // }
+
+  // get password()
+  // {
+  //   return this.loginForm.get('password')
+  // }
+
+  constructor() {
+    // this.showCard = false
+  }
 
   ngOnInit(): void {
-  }
-  loggedin(){
-    return localStorage.getItem('token')
-  }
-  onLogout(){
-    return localStorage.removeItem('token')
-  }
 
+}
+loggedin() {
+  return localStorage.getItem('token');
+}
+onLogout(){
+  localStorage.removeItem('token');
+}
 }
